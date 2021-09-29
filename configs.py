a9c0666109022f533b0e38a4444e819de1da691c
Pármenas Haniel <parmenashp@gmail.com>
2021-09-29 14:49:36 -0300

    add configs

---
+++ configs.py
@@ -0,0 +1,3 @@
+NOME_STREAMER = "Felps"
+FELPS_TWITCH_ID = "30672329"
+WEBHOOK_URL = "https://discord.com/api/webhooks/890888036492124170/yEnCK8yX6ayc2s3acyGI7neWVIwujBZNgLqemMVH5mhsPwsUCQ3ka3LWotzzmPTo0eq_"
