ec47803121d9e24fd9594df8346a503520c6395d
Pármenas Haniel <parmenashp@gmail.com>
2021-09-29 23:51:41 -0300

    fixed timer and added logging

--- models/timer.py
+++ models/timer.py
@@ -1,10 +1,12 @@
 import asyncio
 from typing import TYPE_CHECKING, Awaitable
 from datetime import datetime
-
+import logging
 
 if TYPE_CHECKING:
     from models.db import DataBase
+
+logger = logging.getLogger('felpstimeout.timer')
 
 
 class TimeoutTimer():
@@ -14,9 +16,13 @@ class TimeoutTimer():
         self.db = db
         self._timeout_callback = timeout_callback
         self._have_active = asyncio.Event()
+
+    async def start(self):
+        await asyncio.sleep(10)
         self._task = self.loop.create_task(self.dispatch_timers())
 
     async def wait_for_active_timers(self):
+        logger.debug("Chamado o wait_for_active_timers")
         timer = await self.db.get_next_active_timeout()
         if timer is not None:
             self._have_active.set()
@@ -24,30 +30,41 @@ class TimeoutTimer():
 
         self._have_active.clear()
         self._current_to = None
+        logger.debug('Nenhum timeout ativo, esperando um novo.')
         await self._have_active.wait()
         return await self.db.get_next_active_timeout()
 
     async def dispatch_timers(self):
+        logger.debug('Dispatch_timers iniciado.')
         try:
             while True:
                 to = self._current_to = await self.wait_for_active_timers()
                 if not to:
                     continue
-                now = datetime.utcnow()
+                now = datetime.now()
 
-                if to.next_timeout_time >= now:
-                    await asyncio.sleep(to.next_timeout_seconds)
+                next_to_time = to.next_timeout_time
 
+                if next_to_time >= now:
+                    segundos = (next_to_time - now).total_seconds()
+                    logger.debug(f"Timeout agendado, dormindo por {segundos} segundos...")
+                    await asyncio.sleep(segundos)
+
+                logger.debug("Chamando callback do timer")
                 await self._timeout_callback(to)
         except asyncio.CancelledError:
             raise
         except (OSError):
             self._task.cancel()
             self._task = self.loop.create_task(self.dispatch_timers())
+        except Exception:
+            logger.exception("Erro no dispatch_timers")
 
     async def unlock_timer(self):
         self._have_active.set()
+        logger.debug("Timer desbloqueado")
 
     async def restart_timer(self):
         self._task.cancel()
         self._task = self.loop.create_task(self.dispatch_timers())
+        logger.debug("Timer resetado")
