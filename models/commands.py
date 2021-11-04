51da605a9f7ea976c58adf61412e942cb1dcb52f
Pármenas Haniel <parmenashp@gmail.com>
2021-11-04 01:07:54 -0300

    replacing Lookup with Timeouts

--- models/commands.py
+++ models/commands.py
@@ -3,49 +3,49 @@ from dispike.register.models.options import CommandOption, CommandTypes
 
 
 command_timeout = DiscordCommand(
-    name="timeout", description="Dê timeout nos foras da lei!",
+    name="timeout", description="Dá timeout nos foras da lei!",
     options=[
         CommandOption(
             name="username",
-            description="a pessoa que vai receber o CALA BOCA PUTA",
+            description="A pessoa que vai receber o CALA BOCA PUTA",
             required=True,
             type=CommandTypes.STRING),
         CommandOption(
             name="tempo",
-            description="quanto tempo vai durar?",
+            description="Quanto tempo vai durar? (Exemplos: 300s, 120m, 20d, 3w, 1mo, 2y)",
             required=True,
             type=CommandTypes.STRING),
         CommandOption(
             name="motivo",
-            description="então me diga, qual o motivo desse timeout?",
+            description="Então me diga, qual o motivo desse timeout?",
             required=True,
             type=CommandTypes.STRING)
     ]
 )
 
 command_untimeout = DiscordCommand(
-    name="untimeout", description="Tire os timeouts dos não mais foras da lei!",
+    name="untimeout", description="Tira os timeouts dos não mais foras da lei!",
     options=[
         CommandOption(
             name="username",
-            description="a pessoa que vai deixar de receber o CALA BOCA PUTA",
+            description="A pessoa que vai deixar de receber o CALA BOCA PUTA",
             required=True,
             type=CommandTypes.STRING),
         CommandOption(
             name="motivo",
-            description="cá entre nós, qual o motivo dessa libertação?",
+            description="Cá entre nós, qual o motivo dessa libertação?",
             required=True,
             type=CommandTypes.STRING)
     ]
 )
 
 command_check = DiscordCommand(
-    name="lookup", description="Verifique o histório de timeouts de algum usuário.!",
+    name="timeouts", description="Verifica todos os timeouts ativos ou o histório de timeouts de algum usuário.",
     options=[
         CommandOption(
             name="username",
-            description="o nome da pessoa que você quer bisbilhotar",
-            required=True,
+            description="O nome da pessoa que você quer bisbilhotar",
+            required=False,
             type=CommandTypes.STRING)
     ]
 )
